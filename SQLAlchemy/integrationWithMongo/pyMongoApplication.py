c7a29651759d479517d14fa1a2c952ff9bee4b8a
Marco Maddo (CEO TSSTI Tecnologia) <profmaddo@gmail.com>
2024-05-29 15:30:11 -0400

    PyMongo connection client

--- SQLAlchemy/integrationWithMongo/pyMongoApplication.py
+++ SQLAlchemy/integrationWithMongo/pyMongoApplication.py
@@ -1,12 +1,21 @@
 import pymongo
-# This is a sample Python script.
+# pip install --upgrade pymongo
+# openssl version
+# python3 -c "import requests; print(requests.get('https://www.howsmyssl.com/a/check', verify=False).json()['tls_version'])"
+# /usr/lib/python3/dist-packages/urllib3/connectionpool.py:1020: InsecureRequestWarning: Unverified HTTPS request is being made to host 'www.howsmyssl.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
+#   warnings.warn(
+# TLS 1.3su
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# sudo apt-get install build-essential python3-dev
+
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+    client = pymongo.MongoClient("mongodb+srv://user:secreto@clusterpymongo.topsecret.mongodb.net/")
+
+    VALOR = 0
+
+
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
