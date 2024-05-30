8501695fe44df6a9b90a92a9a529ff120e56dec4
Marco Maddo (CEO TSSTI Tecnologia) <profmaddo@gmail.com>
2024-05-30 16:00:02 -0400

    Bulk insert on mongoDB

--- SQLAlchemy/integrationWithMongo/pyMongoApplication.py
+++ SQLAlchemy/integrationWithMongo/pyMongoApplication.py
@@ -19,7 +19,7 @@ import pymongo
 
 
 def db_connection():
-    client = pymongo.MongoClient("mongodb+srv://maddoliveira:mJkjRzjiSecgXWmL@clusterpymongo.uwk8hxk.mongodb.net/")
+    client = pymongo.MongoClient("mongodb+srv://")
 
     db = client.test
 
